b9055e9943e03ce34e140fcc0191753b4634b20d
shufolislam1 <shufolislam1255@gmail.com>
2024-02-16 20:16:41 +0600

    registrration model done

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+
+ACCOUNT_TYPE=(
+    ('Savings', 'Savings'),
+    ()
+)
+GENDER_TYPE=(
+    ('Male', 'Male'),
+    ('Female', 'Female')
+)
 # Create your models here.
+class UserBankAccount(models.Model):
+    user = models.OneToOneField(User, related_name='account', on_delete= models.CASCADE)
+    account_type = models.CharField(max_length=20, choices=ACCOUNT_TYPE)
+    account_no = models.IntegerField(unique=True)
+    birth_date = models.DateTimeField(null =True, blank=True)
+    gender = models.CharField(max_length=20, choices=GENDER_TYPE)
+    initial_deposite_date = models.DateField(auto_now_add = True)
+    balance = models.DecimalField(default=0, max_digits=12, decimal_places=2)
+    
