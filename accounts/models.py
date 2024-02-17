2d86f5ff90f896935b16c20474585ea8b8b8164e
shufolislam1 <shufolislam1255@gmail.com>
2024-02-17 22:50:03 +0600

    admin panel done

--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 ACCOUNT_TYPE=(
     ('Savings', 'Savings'),
-    ()
+    ('Current', 'Current')
 )
 GENDER_TYPE=(
     ('Male', 'Male'),
@@ -20,3 +20,17 @@ class UserBankAccount(models.Model):
     initial_deposite_date = models.DateField(auto_now_add = True)
     balance = models.DecimalField(default=0, max_digits=12, decimal_places=2)
     
+    def __str__(self):
+        return str(self.account_no)
+    
+    
+class userAddress(models.Model):
+    user = models.OneToOneField(User, related_name = 'address', on_delete = models.CASCADE)
+    street_address = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    postal_code = models.IntegerField()
+    country = models.CharField(max_length = 100)
+    
+    def __str__(self):
+        return str(self.user.email)
+
