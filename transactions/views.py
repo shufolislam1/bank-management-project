d48636d263ac4fb51285fa361bb2f0e6693fcf66
shufolislam1 <shufolislam1255@gmail.com>
2024-02-24 22:45:03 +0600

    base view done

--- transactions/views.py
+++ transactions/views.py
@@ -1,3 +1,26 @@
+from typing import Any
 from django.shortcuts import render
+from django.views.generic import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import  Transaction
 
 # Create your views here.
+
+class TransactionCreateMixin(LoginRequiredMixin, CreateView):
+    template_name=''
+    model = Transaction
+    title = ''
+    success_url =''
+    
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs.update({
+            'account': self.request.user.account,
+        })
+        return kwargs
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'title':self.title
+        })
