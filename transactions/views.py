70a2d82a235f7286b5dc3935da9798bb3e0a6a6a
shufolislam1 <shufolislam1255@gmail.com>
2024-02-25 15:30:03 +0600

    withdraw and loan view done

--- transactions/views.py
+++ transactions/views.py
@@ -6,6 +6,7 @@ from .models import  Transaction
 from .forms import DepositForm, WithdrawForm, LoanRequestForm
 from .constant import DEPOSIT, WITHDRAWAL, LOAN, LOAN_PAID
 from django.contrib import messages
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -47,3 +48,39 @@ class DepositMoneyView(TransactionCreateMixin):
         
         return super().form_valid(form)
     
+    
+    
+class WithdrawMoneyView(TransactionCreateMixin):
+    form_class = WithdrawForm
+    title = 'Withdraw'
+    
+    def get_initial(self):
+        initial = {'transaction_type': WITHDRAWAL}
+        return initial
+    
+    def form_valid(self, form):
+        amount = form.cleaned_data.get('amount')
+        account = self.request.user.account
+        account.balance -= amount
+        account.save(
+            update_fields = ['balance']
+        )
+        
+        return super().form_valid(form)
+    
+    
+class LoanRequestView(TransactionCreateMixin):
+    form_class = LoanRequestForm
+    title = 'Request for loan'
+    
+    def get_initial(self):
+        initial = {'transaction_type': DEPOSIT}
+        return initial
+    
+    def form_valid(self, form):
+        amount = form.cleaned_data.get('amount')
+        current_loan_count = Transaction.objects.filter(account = self.request.user.account, transaction_type = LOAN, loan_approve = True).count()
+        
+        if current_loan_count >=3 :
+            return HttpResponse("You have crossed loan limit")
+        return super().form_valid(form)
