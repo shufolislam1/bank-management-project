dae74c9038a6f77b51d1907c1a7add745860c1fd
shufolislam1 <shufolislam1255@gmail.com>
2024-02-25 18:23:59 +0600

    transaction reporrt vview

--- transactions/views.py
+++ transactions/views.py
@@ -1,12 +1,15 @@
 from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import  Transaction
 from .forms import DepositForm, WithdrawForm, LoanRequestForm
 from .constant import DEPOSIT, WITHDRAWAL, LOAN, LOAN_PAID
 from django.contrib import messages
 from django.http import HttpResponse
+from datetime import datetime
+from django.db import Sum
 
 # Create your views here.
 
@@ -84,3 +87,33 @@ class LoanRequestView(TransactionCreateMixin):
         if current_loan_count >=3 :
             return HttpResponse("You have crossed loan limit")
         return super().form_valid(form)
+    
+    
+class TransactionReportView(LoginRequiredMixin, ListView):
+    template_name = ''
+    model =  Transaction
+    balance = 0
+    
+    def get_queryset(self):
+        queryset = super().get_queryset().filter(
+            account = self.request.user.account
+        )
+        
+        start_date_str = self.request.GET.get('start_date')
+        end_date_str = self.request.GET.get('end_date')
+        
+        if start_date_str and end_date_str:
+            start_date = datetime.strptime(start_date_str, '%Y-%m-%d').date()
+            end_date = datetime.strptime(end_date_str, '%Y-%m-%d').date()
+            queryset = queryset.filter(timestamp_date_gte = start_date, timestamp_date_lte = end_date)
+            
+            self.balance = Transaction.objects.filter(timestamp_date_gte = start_date, timestamp_date_lte = end_date).aggregate(Sum('amount'))['amount_sum']
+        else:
+            self.balance = self.request.user.account.balance
+        return queryset 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'account': self.request.user.account
+        })
+        return context
